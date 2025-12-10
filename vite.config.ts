cf294eac962834e623a7517e9b1414001cd245c4
osmangsnydrbts-tech <osmangsnydrbts@gmail.com>
2025-12-10 04:20:21 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Changed from '/' to './' to support relative paths
+  base: '/', // Absolute path is recommended for standard root deployments
   build: {
+    outDir: 'dist',
     chunkSizeWarningLimit: 1600,
+    assetsDir: 'assets',
+    emptyOutDir: true,
   },
 })
