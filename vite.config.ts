8d2963c80befc04e250f362a32332c3eda8facf9
osmangsnydrbts-tech <osmangsnydrbts@gmail.com>
2025-12-10 03:38:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // مهم جداً لضمان تحميل الملفات بشكل صحيح
+  base: './', // Changed from '/' to './' to support relative paths
   build: {
     chunkSizeWarningLimit: 1600,
   },
